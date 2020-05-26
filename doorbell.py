018c26432fca918bb0a3afdec469cf8100dda9c6
Filip Babuta <filip-razvan.babuta@student.tuiasi.ro>
2020-05-26 13:57:23 +0300

    Final Commit

--- doorbell.py
+++ doorbell.py
@@ -21,7 +21,7 @@ RST = 24
 DC = 23
 SPI_PORT = 0
 SPI_DEVICE = 0
-
+GPIO.setmode(GPIO.BCM)
 trig = 24
 echo = 25
 buzzer=8
@@ -44,67 +44,75 @@ disp.display()
 
 while 1: 
     
-    	disp.clear()
-    	#set the trigger to HIGH
-	GPIO.output(trig, GPIO.HIGH)
-	#sleep 0.00001 s and the set the trigger to LOW
-	time.sleep(0.00001)
-	GPIO.output(trig, GPIO.LOW)
-	#save the start and stop times
-	start = time.time()
-	stop = time.time()
-	#modify the start time to be the last time until
-	#the echo becomes HIGH
-	while GPIO.input(echo) == 0:
-		start = time.time()
-	#modify the stop time to be the last time until
-	#the echo becomes LOW
-	while  GPIO.input(echo) == 1:
-		stop = time.time()
+    disp.clear()
+        #set the trigger to HIGH
+    GPIO.output(trig, GPIO.HIGH)
+    #sleep 0.00001 s and the set the trigger to LOW
+    time.sleep(0.00001)
+    GPIO.output(trig, GPIO.LOW)
+    #save the start and stop times
+    start = time.time()
+    stop = time.time()
+    #modify the start time to be the last time until
+    #the echo becomes HIGH
+    while GPIO.input(echo) == 0:
+        start = time.time()
+    #modify the stop time to be the last time until
+    #the echo becomes LOW
+    while  GPIO.input(echo) == 1:
+        stop = time.time()
 
-	#get the duration of the echo pin as HIGH
-	duration = stop - start
-	#calculate the distance
-	distance = 34300/2 * duration
-    	if distance < 30:
-		smtpUser='test.gyro.sm@gmail.com'
-  		smtpPass='testac123'
-		toAdd='test.gyro.sm@gmail.com'
-  		fromAdd=smtpUser
-		now = datetime.now()
-  		dt_string = now.strftime("%d-%b-%Y (%H:%M:%S)")
-  		#print dt_string
-		msg="The DoorBell has ringed at "
-		subject= msg + dt_string
-		header='To: ' + toAdd + '\n' + 'From: ' + fromAdd + '\n' + 'Subject: ' + subject
-		body= ' From within a Python script'
-		#print header + '\n' + body
-		server=smtplib.SMTP('smtp.gmail.com', 587)
-		server.ehlo()
-		server.starttls()
-		server.ehlo()
-		server.login(smtpUser,smtpPass)
-  		server.sendmail(fromAdd,toAdd, header + '\n' + body)
-  		server.quit()
-      		for i in range(1,3):
-        		disp.clear()
-        		GPIO.output(buzzer,GPIO.HIGH)
-        		img=Image.open('ring2.png').convert('1')
-        		disp.image(img)
-        		disp.diplay()
-        		time.sleep(0.5)
-        		
-			disp.clear()
-        		GPIO.output(buzzer,GPIO.LOW)
-        		img=Image.open('ring3.png').convert('1')
-        		disp.image(img)
-        		disp.diplay()
-			time.sleep(0.5)
-	print ("Distance measured by sensor is  %.2f" % distance)
-	#sleep 0.5 seconds before the next reading
-	time.sleep(0.5)
-		
-		
+    #get the duration of the echo pin as HIGH
+    duration = stop - start
+    #calculate the distance
+    distance = 34300/2 * duration
+    disp.clear()
+    disp.display()
+    print ("Distance is %.2f" %distance)
+    if distance < 30  and distance >5 :
+        print("DoorBell  Rings !")
+        for i in range(1,3):
+                disp.clear()
+                GPIO.output(buzzer,GPIO.HIGH)
+                img=Image.open('ring2.png').convert('1')
+                disp.image(img)
+                disp.display()
+                time.sleep(0.3)
+                
+                disp.clear()
+                GPIO.output(buzzer,GPIO.LOW)
+                img=Image.open('ring3.png').convert('1')
+                disp.image(img)
+                disp.display()
+                time.sleep(0.3)
+        smtpUser='test.gyro.sm@gmail.com'
+        smtpPass='testac123'
+        toAdd='test.gyro.sm@gmail.com'
+        fromAdd=smtpUser
+        now = datetime.now()
+        dt_string = now.strftime("%d-%b-%Y (%H:%M:%S)")
+        #print dt_string
+        msg="The DoorBell has ringed at "
+        subject= msg + dt_string
+        header='To: ' + toAdd + '\n' + 'From: ' + fromAdd + '\n' + 'Subject: ' + subject
+        body= ' From within a Python script'
+        #print header + '\n' + body
+        server=smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(smtpUser,smtpPass)
+        server.sendmail(fromAdd,toAdd, header + '\n' + body)
+        print("Alert send via e-mail")
+        server.quit()
+        
+    disp.clear()
+    disp.display()
+    
+    #sleep 0.5 seconds before the next reading
+    time.sleep(0.5)
+        
+        
     
 
         
